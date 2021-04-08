ebfe5f0217ae4b71a248d65fe49b9c7d7864c80b
FrancescoPierfederici <francescopierf@gmail.com>
2021-04-08 17:07:28 +0200

    commit 1

---
+++ myscript.js
@@ -0,0 +1,6 @@
+var kmPercorsi = parseInt(prompt ("Quanti km vuoi percorrere?"));
+var etàPasseggero = parseInt(prompt ("Età del passeggero"));
+
+if (Number.isNaN(kmPercorsi) || Number.isnNan(etàPasseggero)) {
+   alert("Dati Mancanti o Errati") ;
+}
